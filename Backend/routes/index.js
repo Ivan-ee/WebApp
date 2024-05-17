9d6ef3c75106c044f07fdc330bb26e1fde67677d
Smirnov Ivan <>
2024-05-17 12:55:14 +0300

    add docker 

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const multer = require('multer');
+
 const router = express.Router();
+const uploadDestination = 'uploads';
+
+const storage = multer.diskStorage({
+    destination: uploadDestination,
+    filename: (req, file, callback) => {
+        callback(null, `${Date.now()}.${file.originalname}`);
+    }
+})
 
 router.get('/register', (req, res) => {
     res.send('register')
