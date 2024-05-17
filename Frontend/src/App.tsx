223cf20ff782ea948fd0f3db3ebbe19d7ddea9b1
Smirnov Ivan <>
2024-05-18 02:22:53 +0300

    init frontend files

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import {Button} from "@nextui-org/react";
+
+
+const App = () => {
+    return (
+        <div>
+            <Button color="primary">
+                Button
+            </Button>
+        </div>
+    )
+}
+
+export default App
