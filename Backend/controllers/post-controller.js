fa3c79fa9fa1aa2d5f5f189e6b9772d970571ea9
Smirnov Ivan <>
2024-05-17 21:16:47 +0300

    add get post by id

--- Backend/controllers/post-controller.js
+++ Backend/controllers/post-controller.js
@@ -7,13 +7,11 @@ const PostController = {
 
         const authorId = req.user.userId;
 
-        console.log(authorId)
-
-        if (!content){
+        if (!content) {
             return res.status(400).send({error: "Все поля обязательны"});
         }
 
-        try{
+        try {
 
             const post = await prisma.post.create({
                 data: {
@@ -25,14 +23,63 @@ const PostController = {
             return res.status(200).send(post);
         } catch (error) {
             console.error('err', error)
-            res.status(500).json({error:error});
+            res.status(500).json({error: error});
         }
     },
     getAll: async (req, res) => {
-        res.status(200).send({"message": "get all"});
+        const authorId = req.user.userId;
+
+        try {
+            const posts = await prisma.post.findMany({
+                include: {
+                    author: true,
+                    likes: true,
+                    comments: true,
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                }
+            })
+
+            const postsWithLikeInfo = posts.map(post => ({
+                ...post,
+                likedByUser: post.likes.some(like => like.userId === userId)
+            }));
+
+            res.json(postsWithLikeInfo);
+        } catch (error) {
+            console.error('err', error)
+            res.status(500).json({error: error});
+        }
     },
     getById: async (req, res) => {
-        res.status(200).send({"message": "get by id"});
+        const {id} = req.params ;
+
+        const userId = req.user.userId;
+
+        try {
+            const post = await prisma.post.findUnique({
+                where: {id: id},
+                include: {
+                    author: true,
+                    likes: true,
+                    comments: {
+                        include: {
+                            user: true
+                        }
+                    }
+                }
+            });
+
+            if (!post){
+                return res.status(404).json({error: 'Пост не найден'})
+            }
+
+            return res.status(200).json(post);
+        } catch (error) {
+            console.error('err', error);
+            res.status(500).json({error: error});
+        }
     },
     delete: async (req, res) => {
         res.status(200).send({"message": "delete"});
