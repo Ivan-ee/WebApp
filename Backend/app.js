91a015d2257bc2add7835a47d26349231d954d2c
Smirnov Ivan <>
2024-05-20 10:12:28 +0300

    update docker WTF

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,20 @@
 const  createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const fs = require('fs');
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.set('view engine', 'pug');
 
 app.use('/uploads', express.static('uploads'));
 app.use('/api', require('./routes'));
